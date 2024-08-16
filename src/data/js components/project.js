7d0a8eb2a85d8a885881bb84609eb93b6dc2dd1c
Khizarali07 <khizarali.cs@gmail.com>
2024-08-16 10:38:35 -0700

    new project added

--- src/data/js components/project.js
+++ src/data/js components/project.js
@@ -2,6 +2,8 @@ import ProBox from "./probox.js";
 import project1 from "../media/project1.png";
 import project2 from "../media/project2.png";
 import project3 from "../media/project3.png";
+import project4 from "../media/project4.png";
+import project5 from "../media/project5.png";
 
 function Projects() {
   return (
@@ -36,6 +38,20 @@ function Projects() {
                 description="Built a dynamic currency converter using HTML, CSS, and JavaScript. It leverages an API to fetch real-time exchange rates, allowing you to effortlessly convert between various currencies. Simply enter the amount and choose your desired currencies, and the converter instantly displays the converted value. This project showcases my front-end development skills, including user interaction handling, API integration, and crafting an intuitive interface for a seamless user experience."
                 code="https://github.com/Khizarali07/Currency-Converter"
                 demo="https://khizarali07.github.io/Currency-Converter"
+              />
+              <ProBox
+                title="Calculator"
+                img={project4}
+                description="This project entails the development of a functional calculator web application using HTML, CSS, and JavaScript. The calculator will perform basic arithmetic operations such as addition, subtraction, multiplication, and division e.t.c."
+                code="https://github.com/Khizarali07/Calculator"
+                demo="https://khizarali07.github.io/Calculator"
+              />
+              <ProBox
+                title="Text-Utils"
+                img={project5}
+                description="This React-based application provides a suite of text manipulation tools to enhance user productivity. By leveraging the power of React, the Text Utils app offers a dynamic and interactive interface for users to process text efficiently."
+                code="https://github.com/Khizarali07/Text-Tools"
+                demo="https://khizarali07.github.io/Text-Tools"
               />
             </div>
           </div>
